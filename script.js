87fa07037f3992b89602421a88457b455c5501ba
Gabriel Aguiar Dantas <gabrielaguiardantas@yahoo.com.br>
2022-09-05 18:22:27 -0300

    requisito 5 em desenvolvimento, requisito 6 e 7 ok

--- script.js
+++ script.js
@@ -1,18 +1,21 @@
-function randomColors() {
-  const target = document.getElementsByClassName('color');
-  const button = document.getElementById('button-random-color');
-  function randomBgColor() {
-    const x = Math.random() * 255;
-    const y = Math.random() * 255;
-    const z = Math.random() * 255;
-    const bgColor = `rgb(${x},${y},${z})`;
-    return bgColor;
+function randomBgColor() {
+  const x = Math.random() * 255;
+  const y = Math.random() * 255;
+  const z = Math.random() * 255;
+  const bgColor = `rgb(${x},${y},${z})`;
+  return bgColor;
+} randomBgColor();
+
+const target = document.getElementsByClassName('color');
+const button = document.getElementById('button-random-color');
+
+function qualquer() {
+  for (let i = 1; i < target.length; i += 1) {
+    const backgroundColor = randomBgColor();
+    target[i].style.backgroundColor = backgroundColor;
+    target[i].style.color = backgroundColor;
   }
-  button.addEventListener('click', () => {
-    for (let i = 1; i < target.length; i += 1) {
-      const backgroundColor = randomBgColor();
-      target[i].style.backgroundColor = backgroundColor;
-      target[i].style.color = backgroundColor;
-    }
-  });
+}
+function randomColors() {
+  button.addEventListener('click', qualquer);
 } randomColors();
