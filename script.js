45696215d7f5f95c19657879b6ec27bac662b920
Gabriel Aguiar Dantas <gabrielaguiardantas@yahoo.com.br>
2022-09-06 15:59:52 -0300

    requisito 5 em desenvolvimento, requisito 6,7,8,9 okk

--- script.js
+++ script.js
@@ -1,31 +1,42 @@
+const box1 = document.getElementsByClassName('color')[0];
+const box2 = document.getElementsByClassName('color')[1];
+const box3 = document.getElementsByClassName('color')[2];
+const box4 = document.getElementsByClassName('color')[3];
+box1.style.backgroundColor = 'black';
+box2.style.backgroundColor = 'rgb(0, 150, 152)';
+box3.style.backgroundColor = 'rgb(237, 222, 164)';
+box4.style.backgroundColor = 'rgb(247, 160, 114)';
+
 function randomBgColor() {
   const x = Math.random() * 255;
   const y = Math.random() * 255;
   const z = Math.random() * 255;
   const bgColor = `rgb(${x},${y},${z})`;
   return bgColor;
-} randomBgColor();
+}
 
 const target = document.getElementsByClassName('color');
 const button = document.getElementById('button-random-color');
 
 function paintBoxes() {
-  let saveBackgroundColors = [];
+  const saveBackgroundColors = [];
   for (let i = 1; i < target.length; i += 1) {
     const backgroundColor = randomBgColor();
     target[i].style.backgroundColor = backgroundColor;
-    target[i].style.color = backgroundColor;
-    saveBackgroundColors += target[i].style.backgroundColor;
-    localStorage.setItem('colorPalette', saveBackgroundColors);
+    saveBackgroundColors.push(target[i].style.backgroundColor);
   }
-} paintBoxes();
-function randomColors() {
-  button.addEventListener('click', paintBoxes);
-} randomColors();
+  localStorage.setItem('colorPalette', JSON.stringify(saveBackgroundColors));
+}
+button.addEventListener('click', paintBoxes);
 
-// function retrieveBackgroundInfos() {
-//  localStorage.getItem('colorPalette') = target.style.backgroundColor;
-// }retrieveBackgroundInfos();
+function retrieveBackgroundInfos() {
+  const saveBackground = JSON.parse(localStorage.getItem('colorPalette'));
+  if (localStorage.length > 0) {
+    for (let i = 1; i < target.length; i += 1) {
+      target[i].style.backgroundColor = saveBackground[i - 1];
+    }
+  }
+}
 
 function selectedBoxes(event) {
   const boxElement = document.querySelector('.selected');
@@ -37,7 +48,15 @@ target[1].addEventListener('click', selectedBoxes);
 target[2].addEventListener('click', selectedBoxes);
 target[3].addEventListener('click', selectedBoxes);
 
-function paintBox() {
-  document.getElementsByClassName('color selected')[0].style.backgroundColor = document.querySelector('.pixel');
+const obj = document.getElementsByClassName('color selected')[0].style.backgroundColor;
+const trgt = document.getElementsByClassName('pixel');
+function arrayPixelPaint(event) {
+  event.target.style.backgroundColor = 'blue';
+  console.log(obj);
+} console.log(obj);
+
+for (let i = 0; i < trgt.length; i += 1) {
+  trgt[i].addEventListener('click', arrayPixelPaint);
 }
-document.getElementsByClassName('pixel')[0].addEventListener('click', paintBox);
+
+window.onload = retrieveBackgroundInfos;
