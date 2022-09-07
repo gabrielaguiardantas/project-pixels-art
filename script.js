2ea753ce27efa4b50965010092607ea6f9862e5e
Gabriel Aguiar Dantas <gabrielaguiardantas@yahoo.com.br>
2022-09-07 13:56:53 -0300

    requisito 12 ok, com ajuda de muita gente aprendi a tratar melhor o localStorage

--- script.js
+++ script.js
@@ -1,11 +1,32 @@
+const clearButton = document.querySelector('#clear-board');
 const box1 = document.getElementsByClassName('color')[0];
 const box2 = document.getElementsByClassName('color')[1];
 const box3 = document.getElementsByClassName('color')[2];
 const box4 = document.getElementsByClassName('color')[3];
+const color = document.getElementsByClassName('color');
+const button = document.getElementById('button-random-color');
+const pixel = document.getElementsByClassName('pixel');
+
 box1.style.backgroundColor = 'black';
 box2.style.backgroundColor = 'rgb(0, 150, 152)';
 box3.style.backgroundColor = 'rgb(237, 222, 164)';
 box4.style.backgroundColor = 'rgb(247, 160, 114)';
+
+function localStorageInit() {
+  const arrayEmpty = [];
+  const pixelEmpty = [];
+  if (localStorage.length < 1) {
+    for (let i = 1; i < color.length; i += 1) {
+      arrayEmpty.push(color[i].style.backgroundColor);
+      localStorage.setItem('colorPalette', JSON.stringify(arrayEmpty));
+    }
+    for (let i = 0; i < pixel.length; i += 1) {
+      pixel[i].style.backgroundColor = 'white';
+      pixelEmpty.push(pixel[i].style.backgroundColor);
+      localStorage.setItem('pixelBoard', JSON.stringify(pixelEmpty));
+    }
+  }
+} localStorageInit();
 
 function randomBgColor() {
   const x = Math.random() * 255;
@@ -15,25 +36,21 @@ function randomBgColor() {
   return bgColor;
 }
 
-const target = document.getElementsByClassName('color');
-const button = document.getElementById('button-random-color');
-
 function paintBoxes() {
   const saveBackgroundColors = [];
-  for (let i = 1; i < target.length; i += 1) {
+  for (let i = 1; i < color.length; i += 1) {
     const backgroundColor = randomBgColor();
-    target[i].style.backgroundColor = backgroundColor;
-    saveBackgroundColors.push(target[i].style.backgroundColor);
+    color[i].style.backgroundColor = backgroundColor;
+    saveBackgroundColors.push(color[i].style.backgroundColor);
   }
   localStorage.setItem('colorPalette', JSON.stringify(saveBackgroundColors));
 }
-button.addEventListener('click', paintBoxes);
 
 function retrieveBackgroundInfos() {
-  const saveBackground = JSON.parse(localStorage.getItem('colorPalette'));
   if (localStorage.length > 0) {
-    for (let i = 1; i < target.length; i += 1) {
-      target[i].style.backgroundColor = saveBackground[i - 1];
+    const saveBackground = JSON.parse(localStorage.getItem('colorPalette'));
+    for (let i = 1; i < color.length; i += 1) {
+      color[i].style.backgroundColor = saveBackground[i - 1];
     }
   }
 }
@@ -43,43 +60,41 @@ function selectedBoxes(event) {
   boxElement.classList.remove('selected');
   event.target.classList.add('selected');
 }
-target[0].addEventListener('click', selectedBoxes);
-target[1].addEventListener('click', selectedBoxes);
-target[2].addEventListener('click', selectedBoxes);
-target[3].addEventListener('click', selectedBoxes);
 
-const trgt = document.getElementsByClassName('pixel');
 function arrayPixelPaint(event) {
   const savePixelBoardInfo = [];
   const obj = document.getElementsByClassName('color selected')[0].style.backgroundColor;
   event.target.style.backgroundColor = obj;
-  for (let i = 0; i < trgt.length; i += 1) {
-    savePixelBoardInfo.push(trgt[i].style.backgroundColor);
+  for (let i = 0; i < pixel.length; i += 1) {
+    savePixelBoardInfo.push(pixel[i].style.backgroundColor);
   }
   localStorage.setItem('pixelBoard', JSON.stringify(savePixelBoardInfo));
 }
-for (let i = 0; i < trgt.length; i += 1) {
-  trgt[i].addEventListener('click', arrayPixelPaint);
-}
-const clearButton = document.querySelector('#clear-board');
 
 function clearBoard() {
-  for (let i = 0; i < trgt.length; i += 1) {
-    trgt[i].style.backgroundColor = 'white';
+  for (let i = 0; i < pixel.length; i += 1) {
+    pixel[i].style.backgroundColor = 'white';
   }
 }
-clearButton.addEventListener('click', clearBoard);
 
 function retrievePixelBoardInfo() {
-  const saveBoard = JSON.parse(localStorage.getItem('pixelBoard'));
-  console.log(localStorage);
   if (localStorage.length > 0) {
-    for (let i = 0; i < trgt.length; i += 1) {
-      trgt[i].style.backgroundColor = saveBoard[i];
+    const saveBoard = JSON.parse(localStorage.getItem('pixelBoard'));
+    for (let i = 0; i < pixel.length; i += 1) {
+      pixel[i].style.backgroundColor = saveBoard[i];
     }
   }
 }
 
+color[0].addEventListener('click', selectedBoxes);
+color[1].addEventListener('click', selectedBoxes);
+color[2].addEventListener('click', selectedBoxes);
+color[3].addEventListener('click', selectedBoxes);
+button.addEventListener('click', paintBoxes);
+clearButton.addEventListener('click', clearBoard);
+for (let i = 0; i < pixel.length; i += 1) {
+  pixel[i].addEventListener('click', arrayPixelPaint);
+}
 window.onload = function qualquer() {
   retrieveBackgroundInfos();
   retrievePixelBoardInfo();
